b506791c38cc0eb632c5a3c41f64166a7367234b
Vitalii Khalash <khalash.vitalii@chnu.edu.ua>
2025-05-13 23:12:21 +0300

    chore: merge base project structure into main

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import connectToDB from "./db/connection.js";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+connectToDB();
+
+app.use(cors());
+app.use(express.json());
+
+// Swagger definition
+const swaggerOptions = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "music-api",
+            version: "1.0.0",
+        },
+    },
+    apis: ['./controllers/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Start the Express server
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
