4287e2af737bb96ff4272333d98f973c72d8258d
Vitalii Khalash <khalash.vitalii@chnu.edu.ua>
2025-05-19 20:08:02 +0300

    feat(auth): add authentication middleware to protect private user routes

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,14 @@ import {
     updateCurrentUser,
     deleteCurrentUser,
 } from "../controllers/users.controller.js";
+import validateToken from "../middleware/validateTokenHandler.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/me", fetchCurrentUser);
-router.patch("/me", updateCurrentUser);
-router.delete("/me", deleteCurrentUser);
+router.get("/me", validateToken, fetchCurrentUser);
+router.patch("/me", validateToken, updateCurrentUser);
+router.delete("/me", validateToken, deleteCurrentUser);
 
 export default router;
